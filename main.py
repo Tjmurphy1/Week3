2fce7c9194399c0d9458a877c69401dd28b5949a
Tjmurphy1 <tylermurphy7911@gmail.com>
2022-02-02 15:49:25 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+
+    print("Just doing other  work here")
+    user_response = int(input("What is your age?: "))
+    half_age = user_response/2
+
+
+    print(f"(half_age) is half your age")
